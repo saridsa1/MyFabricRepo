c58766f861f5dcbcb5c0b5fe9879d7da8cf22556
saridsa1 <SARIDSA1@PHINHY-L76342.ap.novartis.net>
2017-03-15 15:37:04 +0530

    Changes for SWAL

--- src/Base.js
+++ src/Base.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
 import './App.css';
 import AppointmentsApp from './App';
-
+import SweetAlert from 'react-swal';
 import {css} from 'glamor';
 import {CompoundButton} from 'office-ui-fabric-react/lib/Button';
 import moment from 'moment';
@@ -9,6 +11,9 @@ import moment from 'moment';
 class BaseApp extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            showAppointmentWarning : false
+        }
     }
 
     componentDidMount() {
@@ -17,6 +22,24 @@ class BaseApp extends Component {
     _noop() {
         console.log("No operation invoked");
     }
+    _appointmentWarningCallback(input){
+        console.log(input);
+        if(input){
+            ReactDOM.render(<AppointmentsApp />, document.getElementById('root'));
+        }
+    }
+    _manageAppointments(){
+        let now = moment();
+        let time = ((now.hour()) >= 12 ? ' PM' : ' AM');
+        console.log(time);
+        if(time.trim() === "PM"){
+           this.setState({
+               showAppointmentWarning: true
+           });
+        } else {
+            ReactDOM.render(<AppointmentsApp />, document.getElementById('root'));
+        }
+    }
 
     render() {
         let dateString = moment(new Date()).format('MMMM Do YYYY, h:mm:ss a');
@@ -24,8 +47,15 @@ class BaseApp extends Component {
         let time = ((now.hour()) >= 12 ? ' PM' : ' AM');
 
         var greeting = (time === "AM")? "Good morning!" : "Good evening!";
-        return (
-            <div className="ms-Grid">
+        return 
+            {this.state.showAppointmentWarning ? 
+                    (<SweetAlert isOpen={true}
+                        type="warning"
+                        confirmButtonText="Yes"
+                        cancelButtonText="No"
+                    text="Appointments are made in the morning, do you still want to continue ?" callback={this._appointmentWarningCallback.bind(this)} />)
+            : 
+            (<div className="ms-Grid">
                 <div className="ms-Grid-row" { ...css({
                     backgroundColor: '#2488d8',
                     boxShadow: '0 0 20px rgba(192, 192, 192, .3)',
@@ -43,12 +73,13 @@ class BaseApp extends Component {
                     <div className="ms-Grid-col ms-u-sm6 ms-u-md4 ms-u-lg4"></div>
                     <div className="ms-Grid-col ms-u-sm6 ms-u-md8 ms-u-lg5">
                          <br/>
-                         <CompoundButton icon='Add' description='Add or delete appointments for today'> Manage appointments </CompoundButton>
+                         <CompoundButton icon='Add' description='Add or delete appointments for today' onClick={this._manageAppointments.bind(this)}> Manage appointments </CompoundButton>
                          <br/>
                          <CompoundButton icon='Add' description='Add, edit or delete patients records'> Manage patients </CompoundButton>
                     </div>
                 </div>
-            </div>);
+            </div>
+            )}
     }
 
 }
